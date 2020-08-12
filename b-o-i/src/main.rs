7422c76d8a4aaa4ee81f10265543d7ceba866b73
niranjana687 <niranjana687@gmail.com>
2020-08-12 10:27:40 +0530

    basics of implementation

---
+++ b-o-i/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).unwrap();
+    let n = n.trim().parse::<i32>().expect("invalid input");
+
+    let mut m = String::new();
+    io::stdin().read_line(&mut m).unwrap();
+    let m = m.trim().parse::<i32>().expect("invalid input");
+
+    let t_res = n - m;
+    if t_res < 0 {
+        println!("{}", t_res*(-1));
+    } else {
+        println!("{}", t_res);
+    }
+
+}
