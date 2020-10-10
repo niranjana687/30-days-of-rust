74923bd323b67e5d4d1982811f1943fae2201506
niranjana687 <niranjana687@gmail.com>
2020-10-10 19:37:27 +0530

    new solution to coin problem

--- arranging-coin/src/main.rs
+++ arranging-coin/src/main.rs
@@ -7,8 +7,17 @@ fn arrange_coins(n: i32) -> i32 {
     //mathemctical soluation
     // n(n+1)/2 sum of natural numbers
     
-    return ((((1 + 8 * n as i64) as f64).sqrt() - 1f64) / 2f64) as i32;
+    // return ((((1 + 8 * n as i64) as f64).sqrt() - 1f64) / 2f64) as i32;
 
-    
+    let mut level = 0;
+    let mut coin = 1;
+    let mut copy = n;
+
+    while copy >= coin {
+        copy -= coin;
+        coin += 1;
+        level += 1;
+    } level 
+
 
 }
