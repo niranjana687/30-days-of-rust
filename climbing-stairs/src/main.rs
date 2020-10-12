b54ed25e5a9f9abc8c36f9f58154cb82d2c6b8ed
niranjana687 <niranjana687@gmail.com>
2020-10-12 19:29:39 +0530

    climbing stairs with fibonacci pattern

--- climbing-stairs/src/main.rs
+++ climbing-stairs/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn climb_stairs(n: i32) -> i32 {
+        //fibonacii number 
+        if n == 1 {
+
+            return 1;
+
+        } else {
+            let mut first = 1;
+            let mut second = 2;
+
+            for i in 3..=n {
+                let mut third = first + second;
+                first = second;
+                second = third;
+            }second
+            
+        }
+        
+}
