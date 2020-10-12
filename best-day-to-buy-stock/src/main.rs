0c11a3b429d6f862edec3e435d9657468b4ef41f
niranjana687 <niranjana687@gmail.com>
2020-10-12 16:16:17 +0530

    doesn't work still yaa

--- best-day-to-buy-stock/src/main.rs
+++ best-day-to-buy-stock/src/main.rs
@@ -3,14 +3,14 @@ fn main() {
 }
 
 fn max_profit(prices: Vec<i32>) -> i32 {
-    let mut buying_price = prices.first().unwrap(); 
+    let mut buying_price = prices[0]; 
     let mut profit = 0; 
 
-    for i in 0..prices.len() {
-        if prices[i] < *buying_price {
-            buying_price = &prices[i];
-        } else if prices[i] - buying_price > profit {
-            profit = prices[i] - buying_price;
+    for i in 1..prices.len() {
+        if prices[i] < buying_price {
+            buying_price = prices[i];
+        } else if prices[i] -  buying_price > profit {
+            profit = prices[i] -  buying_price
         }
     }profit
     
