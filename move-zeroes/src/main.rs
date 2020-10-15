7245cf00724a2ffa1d14b319e2ed99106d470846
niranjana687 <niranjana687@gmail.com>
2020-10-15 10:06:24 +0530

    moved zeroes!

--- move-zeroes/src/main.rs
+++ move-zeroes/src/main.rs
@@ -5,13 +5,17 @@ fn main() {
 
 fn move_zeroes(nums: &mut Vec<i32>) {
     let mut last_nonzero = 0;
-    for i in 0..nums.len() {
+    let len = nums.len() ;
+
+    for i in 0..len {
         if nums[i] != 0 {
+            
+            nums[last_nonzero] = nums[i];
             last_nonzero += 1;
-            nums[last_nonzero] = nums[i]
-        }
+        }   
     }
-    for i in last_nonzero..=nums.len() {
+
+    for i in last_nonzero..len {
         nums[i] = 0;
     }
 }
