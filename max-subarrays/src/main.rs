b232b4a19f6783d7e2ea3324ff646d55aaad81a3
niranjana687 <niranjana687@gmail.com>
2020-10-15 13:14:59 +0530

    Kadane's algorithm failed :c

--- max-subarrays/src/main.rs
+++ max-subarrays/src/main.rs
@@ -3,6 +3,11 @@ fn main() {
 }
 
 
-fn max_sub_array(nums: Vec<i32>) -> i32 {
-        
+fn max_sub_array(nums: Vec<i32>)  {
+        let mut max_so_far = 0;
+        let mut max_ending_here = 0;
+
+        for i in nums.iter() {
+            max_ending_here += i; 
+        }
 }
