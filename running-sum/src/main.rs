59628cfec8369eec392dbba3b289bb68a3ce0c78
niranjana687 <niranjana687@gmail.com>
2020-10-16 12:12:40 +0530

    rust but for c

--- running-sum/src/main.rs
+++ running-sum/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+    let mut res = vec![];
+    let mut sum = 0;
+    
+    for i in nums.iter() {
+        sum += *i;
+        res.push(sum);
+    }res
+}
