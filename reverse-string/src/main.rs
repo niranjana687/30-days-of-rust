fc507c24c360bf77519c63a47b7fb17caee3c654
niranjana687 <niranjana687@gmail.com>
2020-10-17 22:54:05 +0530

    nailed it woohoo!

--- reverse-string/src/main.rs
+++ reverse-string/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 fn reverse_string(s: &mut Vec<char>) {
     let len = s.len();
     
-    for i in 0..=len/2{
+    for i in 0..len/2{
             let end = len - i - 1;
             s.swap(i, end);
         }
